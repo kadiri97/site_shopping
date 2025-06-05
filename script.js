d404dd9f83fc33d6fdcb784e473a1118f1a9b3b7
hafid kadiri <hafidkadiri2204@gmail.com>
2025-06-05 10:58:12 +0100

    add graphic 2

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
+// LINE CHART
+let exp = "Math.sin(x)";
+const xValues = [];
+const yValues = [];
+for (let x = 0; x <= 10; x += 0.1) {
+  xValues.push(x);
+  yValues.push(Math.sin(x)); // Avoid eval
+}
+
+const lineData = [{ x: xValues, y: yValues, mode: "lines" }];
+const lineLayout = { title: "y = sin(x)" };
+Plotly.newPlot("linetest", lineData, lineLayout);
+// PIE CHART
 const xArray = ["Italy", "France", "Spain", "USA", "Argentina"];
 const yArray = [55, 49, 44, 24, 15];
 
-const layout = {title:"World Wide Wine Production"};
-
-const data = [{labels:xArray, values:yArray, type:"pie"}];
-
-Plotly.newPlot("myPlot", data, layout);
+const pieData = [{ labels: xArray, values: yArray, type: "pie" }];
+const pieLayout = { title: "World Wide Wine Production" };
+Plotly.newPlot("pieChart", pieData, pieLayout);
